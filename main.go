fcd95bd00f664cfa9187970d668694e03d72d55e
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-10 17:49:21 +0700

    define gin for initialize router with method in main.go file

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"campaigns-restapi/auth"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	auth.GetUsers()
+	router := gin.Default() //* Initialize router with gin
+	router.GET("/users", auth.GetUsers) //* Define router endpoint with HTTP request method and handler/controller
+	router.Run() //* Running initialized router
 }
