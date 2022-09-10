5036846b3841d6b93dabe14b4e8fde6e18c139f8
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-10 17:48:57 +0700

    define get all user handler with JSON response

--- auth/auth.handler.go
+++ auth/auth.handler.go
@@ -3,11 +3,14 @@ package auth
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"campaigns-restapi/utils"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func GetUsers() {
+func GetUsers(res *gin.Context) {
 	dsn := "root:rootPassword!@tcp(127.0.0.1:3306)/golang-bwa?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) //* Configure gorm
 
@@ -21,9 +24,18 @@ func GetUsers() {
 		
 		db.Find(&users) //* MySql query 'select all from users' from users slice with implement referencing pointer
 
-		for _, user := range users {
-			fmt.Println(user.Name)
-			fmt.Println(user.Email)
+		// for _, user := range users {
+		// 	fmt.Println(user.Name)
+		// 	fmt.Println(user.Email)
+		// }
+		
+		//* Define API response with more organized property
+		response := utils.ApiResponse{
+			Success: true,
+			Data: users,
+			Message: "Get all users successfully",
 		}
+
+		res.JSON(http.StatusOK, response) //* Define response body with JSON 200 status code (OK)
 	}
 }
