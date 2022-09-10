b3a352eefee4c320cabec192751f435d766dba77
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-10 17:07:30 +0700

    calling handler for get all users data

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"campaigns-restapi/auth"
 )
 
 func main() {
-	dsn := "root:rootPassword!@tcp(127.0.0.1:3306)/golang-bwa?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	if (err != nil) {
-		log.Fatal(err.Error())
-	}
-
-	log.Println("Successfully connected to database!")
+	auth.GetUsers()
 }
