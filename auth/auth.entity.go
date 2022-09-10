3561b1c498d7f60ad67e96e25344f721331dbfd6
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-10 17:06:45 +0700

    define user entity with field property name differently from field actual db

---
+++ auth/auth.entity.go
@@ -0,0 +1,16 @@
+package auth
+
+import "time"
+
+type User struct { //* Property must different from db field
+	ID             int
+	Name           string
+	Occupation     string
+	Email          string
+	PasswordHash   string
+	AvatarFileName string
+	Role           string
+	Token          string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
