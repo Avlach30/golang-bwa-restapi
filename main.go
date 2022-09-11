724e4a04e3f41d8e3c580b2a1ea0e027b2004779
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-11 15:03:16 +0700

    define signup endpoint ad main go file

--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"log"
-
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"campaigns-restapi/auth"
 )
 
 func main() {
 	dsn := "root:rootPassword!@tcp(127.0.0.1:3306)/golang-bwa?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if (err != nil) {
 		log.Fatal(err.Error())
 	}
 
 	log.Println("Successfully connected to database!")
+
+	userRepository := auth.NewRepository(db)
+	userService := auth.NewService(userRepository)
+	userHandler := auth.NewUserHandler(userService)
+
+	router := gin.Default()
+	firstVerAPI := router.Group("/api/v1")
+
+	firstVerAPI.POST("/auth/signup", userHandler.SignUpHandler)
+
+	router.Run()
 }
