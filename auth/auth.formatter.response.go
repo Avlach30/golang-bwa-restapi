264acb7c0bb667a898441d961baf1ad5e8a448b3
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-12 16:09:26 +0700

    define format response for data key in response api for login

--- auth/auth.formatter.response.go
+++ auth/auth.formatter.response.go
@@ -7,12 +7,28 @@ type UserFormatterResponse struct {
 	Email      string `json:"email"`
 }
 
+type UserLoginFormatterResponse struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
+	Token string `json:"token"`
+}
+
 func FormatUserSignupResponse(user User) UserFormatterResponse {
 	format := UserFormatterResponse{
-		ID: user.ID,
-		Name: user.Name,
+		ID:         user.ID,
+		Name:       user.Name,
 		Occupation: user.Occupation,
+		Email:      user.Email,
+	}
+
+	return format
+}
+
+func FormatUserLoginResponse(user User) UserLoginFormatterResponse {
+	format := UserLoginFormatterResponse{
+		ID:   user.ID,
 		Email: user.Email,
+		Token: "Sometokenamazing",
 	}
 
 	return format
