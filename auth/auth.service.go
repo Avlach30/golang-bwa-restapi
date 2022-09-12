2cf080f1d2a040de33d0c4c9e11b88c63c831f5e
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-12 18:30:54 +0700

    develop service for generate token for successfully login

--- auth/auth.service.go
+++ auth/auth.service.go
@@ -2,14 +2,15 @@ package auth
 
 import (
 	"errors"
-
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type Service interface {
 	SignUp(input SignUpInput) (User, error)
-	LogIn(input LogInInput) (User, error)
+	LogIn(input LogInInput) (User, string, error)
 	CheckUserAvailabilityByEmail(input SignUpInput) (bool, error)
+	GenerateToken(userId int, email string) (string, error)
 }
 
 type service struct {
@@ -45,28 +46,36 @@ func (service *service) SignUp(input SignUpInput) (User, error) {
 	return newUser, nil
 }
 
-func (service *service) LogIn(input LogInInput) (User, error) {
+func (service *service) LogIn(input LogInInput) (User, string, error) {
 	email := input.Email
 	password := input.Password
 
 	user, err := service.repository.FindByEmail(email)
 	if err != nil {
-		return user, err
+		return user, "", err
 	}
 
 	//* If user not found
 	if user.ID == 0 {
-		return user, errors.New("User with that email not found")
+		return user, "", errors.New("User with that email not found")
 	}
 
 	//* Compare hasing password and input password
 	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
 	//* If error (compare result is different)
 	if err != nil {
-		return user, errors.New("incorrect Password")
+		return user, "", errors.New("incorrect Password")
 	}
 
-	return user, nil
+	userToken, err := service.GenerateToken(user.ID, user.Email)
+	if err != nil {
+		return user, "", errors.New("failed to generate token")
+	}
+
+	//* Call repository for update token
+	service.repository.UpdateTokenValue(userToken, email)
+
+	return user, userToken, nil
 }
 
 func (service *service) CheckUserAvailabilityByEmail(input SignUpInput) (bool, error) {
@@ -83,4 +92,24 @@ func (service *service) CheckUserAvailabilityByEmail(input SignUpInput) (bool, e
 	}
 
 	return false, nil
+}
+
+func (service *service) GenerateToken(userId int, email string) (string, error) {
+
+	//* Generate payload token
+	claim := jwt.MapClaims{
+		"userId": userId,
+		"email": email,
+	}
+
+	//* Generate token with signing method and payload
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	//* Signature token with secret text
+	signedToken, err := token.SignedString([]byte("$2a$08$UcyjEygcPA/XaeUp85sQjuOhithx14/Ai3D5lYPixLrMrSQG2NIFy"))
+	if (err != nil) {
+		return signedToken, err
+	}
+
+	return signedToken, nil
 }
