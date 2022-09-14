ca6cf44cf963302b09326a6352f82172e951080d
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-14 17:10:48 +0700

    develop handler of get all campaigns for process request from client and give them response

---
+++ campaign/campaign.handler.go
@@ -0,0 +1,31 @@
+package campaign
+
+import (
+	"campaigns-restapi/helper"
+	"net/http"
+	"strconv"
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service Service
+}
+
+func NewCampaignHandler(service Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (handler *campaignHandler) GetCampaigns(context *gin.Context) {
+	//* Get value from request query user_id and convert it to int
+	userId, _ := strconv.Atoi(context.Query("user_id")) 
+
+	campaigns, err := handler.service.FindAllCampaigns(userId)
+	if (err != nil) {
+		errorResponse := helper.ApiResponse(false, "Error occured", err.Error())
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	response := helper.ApiResponse(true, "Get all campaigns successfully", campaigns)
+	context.JSON(http.StatusOK, response)
+}
