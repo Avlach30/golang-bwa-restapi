15cbe98e47ee4e9875d5b22935af615439123ddf
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-14 20:01:14 +0700

    develop response format data for get all campaigns successfully

--- campaign/campaign.formatter.response.go
+++ campaign/campaign.formatter.response.go
@@ -1,1 +1,46 @@
 package campaign
+
+type GetCampaignformatterResponse struct {
+	ID               int    `json:"id"`
+	UserId           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	Slug             string `json:"slug"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+//* format response for get each campaign
+func FormatGetCampaignResponse(campaign Campaign) GetCampaignformatterResponse {
+	format := GetCampaignformatterResponse{
+		ID:               campaign.ID,
+		UserId:           campaign.UserId,
+		Name:             campaign.Name,
+		ShortDescription: campaign.ShortDescription,
+		ImageUrl:         "",
+		Slug:             campaign.Slug,
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+	}
+
+	if len(campaign.CampaignImages) > 0 { //* Get imageurl from first index of filename
+		format.ImageUrl = "/" + campaign.CampaignImages[0].FileName
+	}
+
+	return format
+}
+
+//* format response for get all campaigns with call function returned format each campaign
+func FormatGetCampaignsResponse(campaigns []Campaign) []GetCampaignformatterResponse {
+
+	//* Declarating campaigns formatter variable with intial value is empty array
+	campaignsFormatter := []GetCampaignformatterResponse{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatGetCampaignResponse(campaign)           //* Call function for format get each campaign
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter) //* Append called function to array
+	}
+
+	return campaignsFormatter
+}
