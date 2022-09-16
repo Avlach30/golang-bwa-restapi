b74c1c69fe920755372fa475483d010801bb3d22
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-16 17:50:32 +0700

    update repository logic for get all campaigns by specified owner from db

--- campaign/campaign.repository.go
+++ campaign/campaign.repository.go
@@ -37,7 +37,7 @@ func (repository *repository) FindAllByUser(userId int) ([]Campaign, error) {
 	//* Method preload untuk implementasi relasional. param ke 2 opsional untuk kriteria data
 	err := repository.db.Where("user_id = ?", userId).Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
 	if (err != nil) {
-		return campaigns, errors.New("failed to get all campaigns by logged user")
+		return campaigns, errors.New("failed to get all campaigns by specified user")
 	}
 
 	return campaigns, nil
