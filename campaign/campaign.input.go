cad9c3dacc0634daa82fe1e47e1ae2edcf1c9dcd
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-16 17:05:02 +0700

    define struct for input of create new campaign with logged user

--- campaign/campaign.input.go
+++ campaign/campaign.input.go
@@ -1,5 +1,16 @@
 package campaign
 
+import "campaigns-restapi/auth"
+
 type GetSpecifiedCampaignInput struct {
 	ID int `uri:"id" binding:"required"` //* uri key for get value of request params
+}
+
+type CreateNewCampaignInput struct {
+	Name             string `json:"name" binding:"required"`
+	ShortDescription string `json:"short_description"  binding:"required"`
+	Description      string `json:"description"  binding:"required"`
+	GoalAmount       int    `json:"goal_amount"  binding:"required"`
+	Perks            string `json:"perks"  binding:"required"`
+	User             auth.User
 }
