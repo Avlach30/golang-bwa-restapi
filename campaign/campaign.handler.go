9c462459923fb638bc3822c2f421fd75132ca721
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-16 11:29:26 +0700

    develop handler for get request from client input and give response to client it

--- campaign/campaign.handler.go
+++ campaign/campaign.handler.go
@@ -29,3 +29,24 @@ func (handler *campaignHandler) GetCampaigns(context *gin.Context) {
 	response := helper.ApiResponse(true, "Get all campaigns successfully", FormatGetCampaignsResponse(campaigns))
 	context.JSON(http.StatusOK, response)
 }
+
+func (handler *campaignHandler) GetSpecifiedCampaign(context *gin.Context) {
+	var inputId GetSpecifiedCampaignInput
+
+	err := context.ShouldBindUri(&inputId)
+	if (err != nil) {
+		errorResponse := helper.ApiResponse(false, "Error occured", err.Error())
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	campaign, err := handler.service.FindSpecifiedCampaign(inputId)
+	if (err != nil) {
+		errorResponse := helper.ApiResponse(false, "Error occured", err.Error())
+		context.JSON(http.StatusNotFound, errorResponse)
+		return
+	}
+
+	response := helper.ApiResponse(true, "Get specified campaign successfully", FormatGetSpecifiedCampaignResponse(campaign))
+	context.JSON(http.StatusOK, response)
+}
